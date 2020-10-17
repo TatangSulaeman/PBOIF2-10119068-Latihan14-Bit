3d3e28cfc150fbd070ee5dda254f80a51f23f8d3
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:51:06 +0700

    PBOIF2-10119068-Latihan14-Bit

---
+++ src/pboif2/pkg10119068/latihan14/bit/PBOIF210119068Latihan14Bit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan14.bit;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Bit
+ */
+public class PBOIF210119068Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 60; /* 60 = 0011 1100 */
+        int b = 13; /* 13 = 0000 11011 */
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /* -61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+        
+        
+    }
+    
+}
